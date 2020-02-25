6b5074ded4f975676e8ac247018c7c3f2a43080e
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-02-25 15:14:56 -0300

    icone e splash screen

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Routes from './Routes';
 import {Provider} from 'react-redux';
 import store from './store';
+import { YellowBox } from 'react-native';
 
 export default function App() {
+  YellowBox.ignoreWarnings(['Warning: componentWillReceiveProps']);
   return (
     <Provider store={store}>
       <Routes/>
